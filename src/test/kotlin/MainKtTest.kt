3257dc8c1f8c6d23fcb9fe1cb41b37af93f4f44a
Youssef Soliman <yosfsoliman151@gmail.com>
2023-02-14 23:30:40 +0200

    add given when then comments

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -3,39 +3,57 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 class MainKtTest {
-//1-intro to testing
+    //1-intro to testing
+    fun should_ReturnCorrectSummation_When_BothNumbersPositive() {
+        //given  two positive numbeers
+        val firstNumber=3
+        val secondNumber=5
+        //when find the summation of two numbers
+        val sumResult=sumTwoNumbers(3,5)
+        //then
+        assertEquals(8,sumResult)
+    }
+
+    //2-naming functions:
     @Test
-    fun sumTwoNumbersFirstTest() {
-        assertEquals(8,sumTwoNumbers(3,5))
+    fun should_ReturnTrue_When_TheNumberIsEven() {
+        //given even number
+        val numberToCheck=10
+        //when find number is even
+        val result = isEven(numberToCheck)
+        //then
+        assertEquals(true,result)
     }
 
     @Test
-    fun sumTwoNumbersSecondTest() {
-        assertEquals(2,sumTwoNumbers(-3,5))
+    fun should_ReturnFalse_When_TheNumberIsOdd() {
+        //given odd number
+        val numberToCheck=9
+        //when check if number is odd
+        val result=(isEven(numberToCheck))
+        //then
+        assertFalse(result)
     }
 
     @Test
-    fun sumTwoNumbersThirdTest() {
-        assertEquals(-8,sumTwoNumbers(-3,-5))
-    }
-//2-naming functions:
-    @Test
-    fun should_ReturnTrue_When_TheNumberIsEven(){
-        assertEquals(true,isEven(10))
-    }
-
-    @Test
-    fun should_ReturnFalse_When_TheNumberIsOdd(){
-        assertFalse(isEven(9))
+    fun isEven_theNumberIsEven_returnTrue() {
+        //given number
+        val numberToCheck=4
+        //when number is even
+        val result=isEven(numberToCheck)
+        //then
+        assertTrue(result)
     }
 
     @Test
-    fun isEven_theNumberIsEven_returnTrue(){
-        assertTrue(isEven(4))
+    fun isEven_theNumberIsOdd_returnFalse() {
+        //given number
+        val numberToCheck=3
+        //when number is odd
+        val result=isEven(numberToCheck)
+        //then
+        assertFalse(result)
     }
 
-    @Test
-    fun isEven_theNumberIsOdd_returnFalse(){
-        assertFalse(isEven(9))
-    }
+
 }
