a7e120a87974bca7e77c6c1615879d3e103e31a9
Youssef Soliman <yosfsoliman151@gmail.com>
2023-02-15 15:28:38 +0200

    add find average method with test cases and learn assertNull

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ fun main() {
     println(sumTwoNumbers(3,5)==8)//true
     println(sumTwoNumbers(-3,-5)==-8)//true
     println(sumTwoNumbers(-3,5)==2)//true
+    println(getAverageGrowth(18,192))
 }
 
 fun sumTwoNumbers(firstNumber: Int, secondNumber: Int): Int {
@@ -10,3 +11,18 @@ fun sumTwoNumbers(firstNumber: Int, secondNumber: Int): Int {
 }
 
 fun isEven(numberToCheck:Int)=numberToCheck%2==0
+
+fun divide(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber/secondNumber
+}
+
+fun getAverageGrowth(year: Int, height: Int):Int? {
+    return if (year in 1..18 && height in 30..210) {
+        height/year
+    } else {
+        null
+    }
+
+
+
+}
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.function.Executable
 
 class MainKtTest {
     //1-intro to testing
@@ -53,6 +54,106 @@ class MainKtTest {
         val result=isEven(numberToCheck)
         //then
         assertFalse(result)
+    }
+
+    //3-throw exceptions
+    @Test
+    fun should_ThrowException_When_SecondNumberIsZero(){
+        //given two number second number is zero
+        val firstNumber=5
+        val secondNumber=0
+        //when divide by zero
+        val divideResult:Executable=Executable{divide(firstNumber,secondNumber)}
+        //then the exception should be thrown
+        assertThrows(Exception::class.java,divideResult)
+    }
+
+    @Test
+    fun should_ReturnCorrectAverage_when_HaveCorrectInput() {
+        //given correct year and height
+        val age=10
+        val height=110
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertEquals(11,average)
+    }
+@Test
+    fun should_ReturnCorrectAverage_when_TheAverageCanBeRoundedToNextNumber() {
+        //given correct year and height
+        val age=18
+        val height=192
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertEquals(11,average)
+    }
+
+
+    @Test
+    fun should_ReturnMinus1_when_AgeLargerThan18() {
+        //given correct height and wrong age
+        val age=25
+        val height=175
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertNull(average)
+    }
+
+
+    @Test
+    fun should_ReturnMinus1_when_AgeIsZero() {
+        //given  age is zero and correct height
+        val age=0
+        val height=110
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertNull(average)
+    }
+
+    @Test
+    fun should_ReturnMinus1_when_AgeIsNegative() {
+        //given  age is negative and correct height
+        val age=-18
+        val height=110
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertNull(average)
+    }
+
+    @Test
+    fun should_ReturnMinus1_when_HeightSmallerThan30() {
+        //given correct age and short height
+        val age=16
+        val height=25
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertNull(average)
+    }
+    @Test
+    fun should_ReturnMinus1_when_HeightLargerThan210() {
+        //given correct age and tall height
+        val age=16
+        val height=250
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertNull(average)
+    }
+
+    @Test
+    fun should_ReturnMinus1_when_EnterWrongAgeAndHeight() {
+        //given wrong age and short height
+        val age=28
+        val height=2
+        //when calculate the average
+        val average=getAverageGrowth(age,height)
+        //then check the result
+        assertNull(average)
     }
 
 
